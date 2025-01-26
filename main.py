0129ce1d7a7883ec9e5c5d5b4434f4a5b0d2f782
ayman0713 <aymansr94@gmail.com>
2025-01-26 17:12:33 +0000

    First commit TBAG

---
+++ main.py
@@ -0,0 +1,41 @@
+from room import Room
+from character import Enemy, Character
+
+kitchen = Room('kitchen')
+kitchen.set_name('kitchen')
+kitchen.set_description('A dank and dirty room buzzing with flies')
+
+
+ballroom = Room('ballroom')
+ballroom.set_name('ballroom')
+ballroom.set_description('A vast room with a shiny wooden floor')
+
+
+
+dining_hall = Room('dining hall')
+dining_hall.set_name('dining hall')
+dining_hall.set_description('A large room with ornate golden decorations')
+
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom,"west")
+ballroom.link_room(dining_hall,"east")
+
+
+dave = Enemy("Dave", "A smelly zombie")
+dave.set_conversation("Brrlgrh... rgrhl... brains...")
+dave.set_weakness("cheese")
+
+
+dining_hall.set_character(dave)
+
+
+current_room = kitchen
+while True:
+    print('\n')
+    inhabitant = current_room.get_character()
+    if inhabitant is not None:
+        inhabitant.describe()
+    command = input('>')
+    current_room = current_room.move(command)
